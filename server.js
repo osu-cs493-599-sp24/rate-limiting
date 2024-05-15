52183d0688539d3a46db512522a001f3c9cc120e
Rob Hess <hessro@oregonstate.edu>
2024-05-15 11:23:02 -0700

    Add code from 5/15 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,58 @@
 const express = require('express');
 
+const redis = require("redis")
+
 const app = express();
+
 const port = process.env.PORT || 8000;
+
+const redisHost = process.env.REDIS_HOST || "localhost"
+const redisPort = process.env.REDIS_PORT || 6379
+
+const redisClient = redis.createClient({
+  url: `redis://${redisHost}:${redisPort}`
+})
+
+const rateLimitMaxReqs = 5
+const rateLimitWindowMs = 60000
+
+async function rateLimit(req, res, next) {
+  const ip = req.ip
+
+  let tokenBucket
+  try {
+    tokenBucket = await redisClient.hGetAll(ip)
+  } catch (e) {
+    next()
+    return
+  }
+  tokenBucket = {
+    tokens: parseFloat(tokenBucket.tokens) || rateLimitMaxReqs,
+    last: parseInt(tokenBucket.last) || Date.now()
+  }
+
+  const timestamp = Date.now()
+  const ellapsedTimeMs = timestamp - tokenBucket.last
+  const refreshRate = rateLimitMaxReqs / rateLimitWindowMs
+  tokenBucket.tokens += ellapsedTimeMs * refreshRate
+  tokenBucket.tokens = Math.min(rateLimitMaxReqs, tokenBucket.tokens)
+  tokenBucket.last = timestamp
+
+  if (tokenBucket.tokens >= 1) {
+    tokenBucket.tokens -= 1
+    await redisClient.hSet(ip, [
+      ['tokens', tokenBucket.tokens],
+      ['last', tokenBucket.last]
+    ])
+    next()
+  } else {
+    res.status(429).send({
+      err: "Too many requests per minute"
+    })
+  }
+}
+
+app.use(rateLimit)
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -15,6 +66,8 @@ app.use('*', (req, res, next) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("== Server is running on port", port);
-});
+redisClient.connect().then(() => {
+  app.listen(port, () => {
+    console.log("== Server is running on port", port);
+  });
+})
